a18610f0d8bc0e7e8c64100b98e78af59100ad84
raihan512 <raihangazi1024@gmail.com>
2023-03-08 14:16:50 +0600

    show ordered item in my orders page class 7 done

--- src/Pages/MyOrders/MyOrders.js
+++ src/Pages/MyOrders/MyOrders.js
@@ -1,5 +1,8 @@
 import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
+import checkoutImg from "../../../src/assets/images/checkout/checkout.png";
+import { Link } from "react-router-dom";
+import OrderItem from "./OrderItem/OrderItem";
 
 const MyOrders = () => {
   const [orders, setOrders] = useState([]);
@@ -12,8 +15,35 @@ const MyOrders = () => {
       .catch((error) => console.log(error));
   }, [user?.email]);
   return (
-    <div>
-      <h3>You have total {orders.length} items</h3>
+    <div className="my-8">
+      {/* Service page banner start */}
+      <div
+        className="hero rounded-md overflow-hidden mt-10 mb-20 relative"
+        style={{
+          backgroundImage: `url("${checkoutImg}")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60 py-20 px-5">
+          <h3 className="mb-5 text-5xl font-bold text-white">
+            Service Details
+          </h3>
+          <p className="text-base text-orange-400">
+            <Link to="/">Home</Link> - My Orders
+          </p>
+        </div>
+      </div>
+      {/* Service page banner end */}
+      {/* Orders Table Start */}
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          <tbody>
+            {orders.map((order) => (
+              <OrderItem key={order._id} order={order}></OrderItem>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {/* Orders Table End */}
     </div>
   );
 };
--- src/Pages/MyOrders/OrderItem/OrderItem.js
+++ src/Pages/MyOrders/OrderItem/OrderItem.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const OrderItem = () => {
-  return <div></div>;
+const OrderItem = ({ order }) => {
+  const { serviceName, price, service } = order;
+  const [orderService, setOrderService] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5000/service/${service}`)
+      .then((res) => res.json())
+      .then((data) => setOrderService(data))
+      .catch((error) => console.error(error));
+  }, [service]);
+  return (
+    <tr>
+      <th>
+        <button className="p-2 text-white bg-black rounded-full border-black">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </th>
+      <td>
+        <div className="avatar">
+          <div className="rounded-md w-32 h-32">
+            <img src={orderService.img} alt="Service" />
+          </div>
+        </div>
+      </td>
+      <td>
+        <div className="font-bold">{serviceName}</div>
+      </td>
+      <td>
+        <p>{price}</p>
+      </td>
+      <th>
+        <button className="btn btn-ghost btn-xs">details</button>
+      </th>
+    </tr>
+  );
 };
 
 export default OrderItem;
